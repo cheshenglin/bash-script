bfc8405685941e6c649060c27928fd33d562b2de
cheshenglin <chesheng@gmail.com>
2019-08-01 19:01:11 +0800

    [FIX] password and output directory

--- backup_databases.sh
+++ backup_databases.sh
@@ -4,14 +4,11 @@ USER="root"
 PASSWORD=""
 PREFIX="mysql"
 
-if [ "$1" != "" ]; then
-    WORKING_DIR=$1
-else
-    WORKING_DIR=$(pwd)
-fi
+if [ "$1" != "" ]; then WORKING_DIR=$1
+else WORKING_DIR=$(pwd); fi
 
-databases=$(mysql -h $HOST -u $USER -p$PASSWORD -e 'show databases;' 2> /dev/null | awk '{print $1}' | tail -n +2)
+databases=$(mysql -h $HOST -u $USER -p'${PASSWORD}' -e 'show databases;' 2> /dev/null | awk '{print $1}' | tail -n +2)
 for dbname in $databases; do
     if [[ $dbname != "$PREFIX"* ]] ; then continue ; fi
-    $(mysqldump -h $HOST -u $USER -p$PASSWORD --databases $dbname > ${WORKING_DIR}${dbname}_`date +%Y%m%d`.sql 2> /dev/null)
+    $(mysqldump -h $HOST -u $USER -p'${PASSWORD}' --databases $dbname > ${WORKING_DIR}/${dbname}_`date +%Y%m%d`.sql 2> /dev/null)
 done
